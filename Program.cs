6fcbd8fd5d8c90af1c2edee8e549fde2f917de75
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-11-02 22:35:25 +0900

    제목 : C# 구구단 만들기 날짜 : 2022. 11. 02 작성 : 강충

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+        }
+    }
+}
