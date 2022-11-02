6f918d6b99d0a120fae240c0c3dd593a2f566c85
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-11-02 22:37:20 +0900

    제목 : 별찍기 날짜 : 2022. 11. 02 작성 : 강충

--- Program.cs
+++ Program.cs
@@ -6,12 +6,13 @@ namespace CSharp
     {
         static void Main(string[]args)
         {
-            for(int i = 1; i < 10; i++)
+            for(int i = 0; i < 5; i++)
             {
-                for(int j = 1; j < 10; j++)
+                for(int j = 0; j <= i; j++)
                 {
-                    Console.WriteLine($"{i} * {j} = {i * j}");
+                    Console.Write("*");
                 }
+                Console.WriteLine();
             }
         }
     }
